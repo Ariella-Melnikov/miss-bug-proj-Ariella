017e86e4dfe071af0db82ce795766cbfbface6eb
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-01 19:05:14 +0300

    add app server+ service+ app front

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
-
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
 
+//* Express Config:
+app.use(express.static('public'))
 
-app.get('/', (req, res) => { 
-    res.send('Hello there')
+//* Express Routing:
+app.get('/api/bug', (req, res) => {
+  const filterBy = {
+    txt: req.query.txt,
+    minSeverity: +req.query.minSeverity,
+  }
+  // res.send('Hello there')
+  bugService
+    .query(filterBy)
+    .then((bugs) => res.send(bugs))
+    .catch((err) => {
+      loggerService.error('Cannot get bugs', err)
+      res.status(500).send('Cannot get bugs')
+    })
 })
 
 const port = 3030
